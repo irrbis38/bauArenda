874e63f2c40397e7eb13ab9d06800520327a832e
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2021-10-01 14:54:46 +0300

    feat: added the logic of the 'show more' button

--- js/app.js
+++ js/app.js
@@ -144,7 +144,7 @@ const feedbackNameAll = document.querySelectorAll(".feedback__name");
 const feedbackPhoneAll = document.querySelectorAll(".feedback__phone");
 
 const removeOnFocus = (el) => {
-  el.addEventListener("focus", (e) => {
+  el.addEventListener("input", (e) => {
     el.parentElement.classList.remove("error");
     el.parentElement.parentElement.children[0].classList.remove("show");
   });
@@ -152,3 +152,403 @@ const removeOnFocus = (el) => {
 
 feedbackNameAll.forEach(removeOnFocus);
 feedbackPhoneAll.forEach(removeOnFocus);
+
+// Показать больше
+
+let catalog = [
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+  {
+    header: "Аренда экскаваторов CATERPILLAR",
+    name1: "Объём фронтального ковша",
+    value1: "1,3 м3",
+    name2: "Высота выгрузки",
+    value2: "2690 мм",
+    name3: "Мощность двигателя",
+    value3: "100 л.с.",
+    price: "100 BYN",
+  },
+];
+
+const catalogWrapper = document.querySelector(".catalog__wrapper");
+
+const createElements = (arr) => {
+  let catalog = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    catalog[i] = document.createElement("DIV");
+    catalog[i].classList.add("catalog__item");
+    catalog[i].innerHTML = `<a href="#" class="catalog__link"></a>
+
+    <div class="catalog__img">
+      <img src="./img/catalog/${i + 1}.jpg" alt="экскаватор" />
+    </div>
+
+    <a href="#" class="catalog__button">Подробнее</a>
+
+    <div class="catalog__info">
+      <h4 class="catalog__header">${arr[i].header}</h4>
+
+      <div class="catalog__options">
+        <div class="catalog__feature">
+          <p class="catalog__name">${arr[i].name1}</p>
+          <p class="catalog__value">${arr[i].value1}</p>
+        </div>
+
+        <div class="catalog__feature">
+          <p class="catalog__name">${arr[i].name2}</p>
+          <p class="catalog__value">${arr[i].value2}</p>
+        </div>
+
+        <div class="catalog__feature">
+          <p class="catalog__name">${arr[i].name3}</p>
+          <p class="catalog__value">${arr[i].value3}</p>
+        </div>
+      </div>
+      <!-- /.catalog__info -->
+
+      <p class="catalog__price">${arr[i].price}</p>
+    </div>
+    <!-- /.catalog__info -->`;
+  }
+
+  return catalog;
+};
+
+let items = createElements(catalog);
+
+let counter = 0;
+
+const addToPage = (items) => {
+  for (let i = counter; i < counter + 16; i++) {
+    if (items[i] !== undefined) {
+      catalogWrapper.appendChild(items[i]);
+    }
+  }
+  counter += 16;
+};
+
+const catalogMore = document.querySelector(".catalog__more");
+catalogMore.addEventListener("click", () => {
+  addToPage(items);
+});
+
+addToPage(items);
